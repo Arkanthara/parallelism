67516e481a177d99a2679764ddeb32faa31404dc
Fitzwilliam Darcy <fitzwilliam@darcy>
2023-10-25 15:47:24 +0200

    Add comments on code

--- exercises.cpp
+++ exercises.cpp
@@ -6,30 +6,36 @@ using namespace std;
 
 int exercise_1(void) {
 
+        // Initialize MPI environment
         MPI_Init(NULL, NULL);
 
         MPI_Status status;
 
+        // Get the rank of the processor and the total number of processors
         int world_rank = 0;
         MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
         int world_size = 0;
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+        // Initialize the message
         char * text = "Hello world !";
         int size = strlen(text);
 
+        // Send message from processor 0 to other processors
         if (world_rank == 0) {
                 for (int i = 1; i < world_size; i++) {
                         MPI_Send(text, size, MPI_CHAR, i, 0, MPI_COMM_WORLD);
                 }
         }
+        // Receive message by other processors
         else {
                 char buffer[size];
                 MPI_Recv(buffer, size, MPI_CHAR, 0, 0, MPI_COMM_WORLD, &status);
                 cout << "Message received by processor " << world_rank << " from processor 0: " << buffer << endl;
         }
 
+        // Destroy MPI environment
         MPI_Finalize();
 
         return 0;
@@ -37,13 +43,16 @@ int exercise_1(void) {
 
 int exercise_2(void) {
 
+        // Initialize MPI environment
         MPI_Init(NULL, NULL);
 
         MPI_Status status;
 
+        // Get the rank of the processor
         int world_rank = 0;
         MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+        // Get the total number of processors
         int world_size = 0;
         MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
@@ -54,10 +63,12 @@ int exercise_2(void) {
         // Buffer for receive message
         char buffer[size];
 
+        // Send message to next processor and receive message from previous processor
         MPI_Send(text, size, MPI_CHAR, (world_rank + 1) % world_size, 0, MPI_COMM_WORLD);
         MPI_Recv(buffer, size, MPI_CHAR, (world_rank + world_size - 1) % world_size, 0, MPI_COMM_WORLD, &status);
         cout << "Processor " << world_rank <<  " receive from processor " << (world_rank + world_size - 1) % world_size << " the message: " << buffer << endl;
 
+        // Destroy MPI environment
         MPI_Finalize();
 
         return 0;
@@ -67,23 +78,26 @@ int exercise_3(void) {
 
 	MPI_Status status;
 
+        // Initialize MPI environment
 	MPI_Init(NULL, NULL);
 
+        // Get the rank of the processor
 	int world_rank = 0;
-
 	MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
 
+        // Get the total number of processors
 	int world_size = 0;
-
 	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
+        // Initialize message to send and buffer
 	char * text = "Hello world !";
 	int size = strlen(text);
 
 	char buffer[size];
         
 	int mask = 1;
-        
+
+        // Send message to processor which rank is different from just one bit of the current processor if the other processor's rank is smaller, else send message to this processor
 	while (world_size > mask) {
 		int process = world_rank ^ mask;
 		if (process > world_rank) {
@@ -96,6 +110,7 @@ int exercise_3(void) {
 		mask = mask << 1;
 	}
 
+        // Destroy MPI environment
 	MPI_Finalize();
 
 	return 0;
--- main.cpp
+++ main.cpp
@@ -6,13 +6,16 @@ using namespace std;
 
 int main(int argc, char * argv[]) {
 
+        // Verify that we have good arguments
 	if (argc != 2) {
 		cerr << "Error !" << endl << "Usage of the program: <command to execute the program> <number of the exercise>" << endl << "Example: runmpi tp2 2" << endl;
 		return -1;
 	}
 
+        // Convert argument to number
 	int exercise_number = atoi(argv[1]);
 
+        // Switch argument for execute exercise number given
 	switch (exercise_number) {
 		case 1:
 			exercise_1();
