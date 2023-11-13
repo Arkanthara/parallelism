e0989891b8294cfab3abfd63a325a6b1e8c19031
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2023-11-13 11:01:23 +0100

    Backup

---
+++ heastSeq/run.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+#SBATCH --job-name Michel_TP2          # Permit us to find easily our job
+#SBATCH --output ./out/Michel_TP2-out.o%j    # Outputs will be written here
+#SBATCH --error ./err/Michel_TP2-err.e%j     # Errors will be written here
+#SBATCH --ntasks 4                    # Number of tasks in our job
+#SBATCH --cpus-per-task 1              # Number of cpus per tasks
+#SBATCH --partition debug-cpu          # Partition to use
+#SBATCH --time 15:00                   # Maximum time execution
+
+# Load modules for compiling and run program
+module load foss
+module load CUDA
+
+echo $SLURM_NODELIST
+
+# Compile program
+make
+# Run program. If the parameter is not given to execute an exercise, we print an error which will be in ./err/Michel_TP2-err.e%j
+# Else, we execute program
+if [ -n "$1" ]; then
+	srun --mpi=pmi2 ./tp3 $1 1
+	srun --mpi=pmi2 ./tp3 $1 10
+	srun --mpi=pmi2 ./tp3 $1 100
+	srun --mpi=pmi2 ./tp3 $1 1000
+	srun --mpi=pmi2 ./tp3 $1 10000
+	srun --mpi=pmi2 ./tp3 $1 100000
+else
+	echo "Error ! Usage of the script: ./run.sh [number of the exercise]" 1>&2
+fi
