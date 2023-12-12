86015cfd415e6f6ba0b9073227f781aa6a0faafc
Fitzwilliam Darcy <Fitzwilliam@Darcy>
2023-12-12 13:38:18 +0100

    Add results execution time

--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@
 #SBATCH --job-name Michel_TP6			# Permit us to find easily our job
 #SBATCH --output ./out/Michel_TP6-out.o%j	# Outputs will be written here
 #SBATCH --error ./err/Michel_TP6-err.e%j	# Errors will be written here
-#SBATCH --nodes 1				# Number of nodes used (on baobab, a node allow to use 14 cores)
 #SBATCH --ntasks 1				# Number of tasks in our job
 #SBATCH --partition debug-cpu			# Partition to use
 #SBATCH --time 15:00				# Maximum time execution
