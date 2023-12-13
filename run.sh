f55f80f08c0b19b874a2a6518acc39a2f9887c98
Fitzwilliam Darcy <Fitzwilliam@Darcy>
2023-12-13 14:17:10 +0100

    Backup

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 #SBATCH --ntasks 1				# Number of tasks in our job
 #SBATCH --cpus-per-task 32
 #SBATCH --partition shared-cpu			# Partition to use
-#SBATCH --time 15:00				# Maximum time execution
+#SBATCH --time 30:00				# Maximum time execution
 
 # Load modules for compiling and run program
 module load foss
@@ -23,27 +23,29 @@ echo $SLURM_NODELIST
 # fi
 # export OMP_NUM_THREADS=$omp_threads
 
-if [ -n "$1" ]; then
-	ITERATIONS = $1
-else
-	ITERATIONS = 100
-fi
+# iter=100
+# 
+# if [ -n "$1" ]; then
+# 	iter=$1
+# fi
 
 # Run program
-for i in [1, 2, 4, 8, 16, 32, 64, 128, 256, 1024, 2048]; do
-	for j in [1, 2, 4, 8, 16, 32, 64]; do
-		make define_chunk_size CHUNK_SIZE=$(i)
-		make
-		echo "Region 1"
-		./tp6_static -i $(ITERATIONS) -n $(i)
-		./tp6_dynamic -i $(ITERATIONS) -n $(i)
-		
-		echo "Region 2"
-		./tp6_static -c 0.3 0.3 0.5 0.5 -i $(ITERATIONS) -n $(i)
-		./tp6_dynamic -c 0.3 0.3 0.5 0.5 -i $(ITERATIONS) -n $(i)
-		
-		echo "Region 3"
-		./tp6_static -c -2 -2 2 2 -i $(ITERATIONS) -n $(i)
-		./tp6_dynamic -c -2 -2 2 2 -i $(ITERATIONS) -n $(i)
+for k in 64 128 256 512 1024; do
+	for i in 1 2 4 8 16 32 64 128 256 512 1024 2048; do
+		for j in 1 2 4 8 16 32 64 128; do
+			make define_chunk_size CHUNK_SIZE=$i
+			make
+			echo "Region 1"
+			./tp6_static -i $k -n $j
+			./tp6_dynamic -i $k -n $j
+			
+			echo "Region 2"
+			./tp6_static -c 0.3 0.3 0.5 0.5 -i $k -n $j
+			./tp6_dynamic -c 0.3 0.3 0.5 0.5 -i $k -n $j
+			
+			echo "Region 3"
+			./tp6_static -c -2 -2 2 2 -i $k -n $j
+			./tp6_dynamic -c -2 -2 2 2 -i $k -n $j
+		done
 	done
 done
--- src/global.hpp
+++ src/global.hpp
@@ -1,6 +1,6 @@
 #ifndef __GLOBAL_HPP__
 #define __GLOBAL_HPP__
 
-const int chunk_size = ;
+const int chunk_size = 256;
 
 #endif
