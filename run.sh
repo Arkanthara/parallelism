77cadb41cd3fc20f5af759a94ba0a0955433f8f2
Fitzwilliam Darcy <Fitzwilliam@Darcy>
2023-12-13 11:07:48 +0100

    Backup

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@
 #SBATCH --output ./out/Michel_TP6-out.o%j	# Outputs will be written here
 #SBATCH --error ./err/Michel_TP6-err.e%j	# Errors will be written here
 #SBATCH --ntasks 1				# Number of tasks in our job
-#SBATCH --partition debug-cpu			# Partition to use
+#SBATCH --cpus-per-task 32
+#SBATCH --partition shared-cpu			# Partition to use
 #SBATCH --time 15:00				# Maximum time execution
 
 # Load modules for compiling and run program
@@ -18,22 +19,22 @@ make
 # Set OMP_NUM_THREADS to the same value as --cpus-per-task
 # with a fallback in case it isn't set.
 # SLURM_CPUS_PER_TASK is set to the value of --cpus-per-task, but only if --cpus-per-task is explicitly set
-if [ -n "$SLURM_CPUS_PER_TASK" ]; then
-  omp_threads=$SLURM_CPUS_PER_TASK
-else
-  omp_threads=1
-fi
-export OMP_NUM_THREADS=$omp_threads
+# if [ -n "$SLURM_CPUS_PER_TASK" ]; then
+#   omp_threads=$SLURM_CPUS_PER_TASK
+# else
+#   omp_threads=1
+# fi
+# export OMP_NUM_THREADS=$omp_threads
 
 # Run program
 echo "Region 1"
-./tp6_static
-./tp6_dynamic
+./tp6_static -i 1024 -n 32
+./tp6_dynamic -i 1024 -n 32
 
 echo "Region 2"
-./tp6_static -c 0.3 0.3 0.5 0.5
-./tp6_dynamic -c 0.3 0.3 0.5 0.5
+./tp6_static -c 0.3 0.3 0.5 0.5 -i 1024 -n 32
+./tp6_dynamic -c 0.3 0.3 0.5 0.5 -i 1024 -n 32
 
 echo "Region 3"
-./tp6_static -c -2 -2 2 2
-./tp6_dynamic -c -2 -2 2 2
+./tp6_static -c -2 -2 2 2 -i 1024 -n 32
+./tp6_dynamic -c -2 -2 2 2 -i 1024 -n 32
