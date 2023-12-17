c35d35b05e454331666b640f35787af912aad647
Fitzwilliam Darcy <Fitzwilliam@Darcy>
2023-12-17 16:20:21 +0100

    Backup

--- heastSeq/runcpu.sh
+++ heastSeq/runcpu.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-#SBATCH --job-name Michel_TP6			# Permit us to find easily our job
-#SBATCH --output ./out/Michel_TP6-out.o%j	# Outputs will be written here
-#SBATCH --error ./err/Michel_TP6-err.e%j	# Errors will be written here
+#SBATCH --job-name Michel_TP7			# Permit us to find easily our job
+#SBATCH --output ./out/Michel_TP7-out.o%j	# Outputs will be written here
+#SBATCH --error ./err/Michel_TP7-err.e%j	# Errors will be written here
 #SBATCH --ntasks 1				# Number of tasks in our job
 #SBATCH --cpus-per-task 32
 #SBATCH --partition shared-cpu			# Partition to use
@@ -13,7 +13,6 @@ module load tbb
 
 echo $SLURM_NODELIST
 
-make cleanall
 make cpu
 
 ./tp7 -i 256 -s 512
--- heastSeq/rungpu.sh
+++ heastSeq/rungpu.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
-#SBATCH --job-name Michel_TP6			# Permit us to find easily our job
-#SBATCH --output ./out/Michel_TP6-out.o%j	# Outputs will be written here
-#SBATCH --error ./err/Michel_TP6-err.e%j	# Errors will be written here
+#SBATCH --job-name Michel_TP7			# Permit us to find easily our job
+#SBATCH --output ./out/Michel_TP7-out.o%j	# Outputs will be written here
+#SBATCH --error ./err/Michel_TP7-err.e%j	# Errors will be written here
 #SBATCH --ntasks 1				# Number of tasks in our job
-#SBATCH --cpus-per-task 32			# Number of cpus per task
-#SBATCH --partition public-gpu			# Partition to use
+#SBATCH --gpus 1
+#SBATCH --partition shared-gpu			# Partition to use
 #SBATCH --time 15:00				# Maximum time execution
 
 # Load modules for compiling and run program
@@ -13,7 +13,6 @@ module load CUDA
 
 echo $SLURM_NODELIST
 
-make cleanall
 make gpu
 
 ./tp7 -i 256 -s 512
