61c36ba5d80d535f25ccc40b873495eceb590f2a
Fitzwilliam Darcy <Elizabeth.Bennet@Fitzwilliam.Darcy>
2023-12-17 15:31:21 +0100

    Backup

--- heastSeq/run.sh
+++ heastSeq/run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-#SBATCH --job-name Michel_TP2          # Permit us to find easily our job
-#SBATCH --output ./out/Michel_TP2-out.o%j    # Outputs will be written here
-#SBATCH --error ./err/Michel_TP2-err.e%j     # Errors will be written here
-#SBATCH --partition debug-cpu          # Partition to use
-#SBATCH --time 15:00                   # Maximum time execution
+#SBATCH --job-name Michel_TP6			# Permit us to find easily our job
+#SBATCH --output ./out/Michel_TP6-out.o%j	# Outputs will be written here
+#SBATCH --error ./err/Michel_TP6-err.e%j	# Errors will be written here
+#SBATCH --ntasks 1				# Number of tasks in our job
+#SBATCH --cpus-per-task 32
+#SBATCH --partition shared-cpu			# Partition to use
+#SBATCH --time 15:00				# Maximum time execution
 
 # Load modules for compiling and run program
 module load foss
@@ -11,31 +13,4 @@ module load CUDA
 
 echo $SLURM_NODELIST
 
-# Compile program
-make
-# Run program. If the parameter is not given to execute an exercise, we print an error which will be in ./err/Michel_TP2-err.e%j
-if [ -n "$1" ]; then
-	echo "$1 64x64 10^0 to 10^5 iterations"
-	srun --mpi=pmi2 ./tp3 64 1
-	srun --mpi=pmi2 ./tp3 64 10
-	srun --mpi=pmi2 ./tp3 64 100
-	srun --mpi=pmi2 ./tp3 64 1000
-	srun --mpi=pmi2 ./tp3 64 10000
-	srun --mpi=pmi2 ./tp3 64 100000
-	echo "$1 128x128 10^0 to 10^5 iterations"
-	srun --mpi=pmi2 ./tp3 128 1
-	srun --mpi=pmi2 ./tp3 128 10
-	srun --mpi=pmi2 ./tp3 128 100
-	srun --mpi=pmi2 ./tp3 128 1000
-	srun --mpi=pmi2 ./tp3 128 10000
-	srun --mpi=pmi2 ./tp3 128 100000
-	echo "$1 256x256 10^0 to 10^5 iterations"
-	srun --mpi=pmi2 ./tp3 256 1
-	srun --mpi=pmi2 ./tp3 256 10
-	srun --mpi=pmi2 ./tp3 256 100
-	srun --mpi=pmi2 ./tp3 256 1000
-	srun --mpi=pmi2 ./tp3 256 10000
-	srun --mpi=pmi2 ./tp3 256 100000
-else
-	echo "Usage: sbatch -n nb run.sh nb"
-fi
+make cpu
