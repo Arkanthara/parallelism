4e911c7d92d01b9d589faf87a9d42beb27085e9c
Fitzwilliam Darcy <Fitzwilliam@Darcy>
2023-12-18 10:50:33 +0100

    Fix portability

--- heastSeq/rungpu.sh
+++ heastSeq/rungpu.sh
@@ -2,16 +2,18 @@
 #SBATCH --job-name Michel_TP7			# Permit us to find easily our job
 #SBATCH --output ./out/Michel_TP7-out.o%j	# Outputs will be written here
 #SBATCH --error ./err/Michel_TP7-err.e%j	# Errors will be written here
-#SBATCH --ntasks 1				# Number of tasks in our job
 #SBATCH --gpus 1
 #SBATCH --partition shared-gpu			# Partition to use
 #SBATCH --time 15:00				# Maximum time execution
 
 # Load modules for compiling and run program
-module load foss/2020b
-module load CUDA
+# module load foss/2018b
+# module load NVHPC/21.9
+module load foss/2018b
+# module load CUDA
+module load NVHPC/21.9
 
-echo $SLURM_NODELIST
+echo $CUDA_VISIBLE_DEVICES
 
 make gpu
 
--- heastSeq/src/heastSeq.cpp
+++ heastSeq/src/heastSeq.cpp
@@ -76,19 +76,6 @@ int main(int argc, char * argv[]) {
 		return grid;
 	};
 
-	// Allow to print a grid.
-	// Usefull to verify if the initialization of the grid is correct
-	auto print_grid = [](vector<double> grid, int size) {
-		int index = 0;
-		for (int i = 0; i < size; i ++) {
-			for (int j = 0; j < size; j ++) {
-				cout << grid[index] << " ";
-				index ++;
-			}
-			cout << endl;
-		}
-	};
-
 	// Initialize a grid to stock results of heast equation
 	vector<double> grid_2 (size * size, 0.);
 
@@ -96,14 +83,14 @@ int main(int argc, char * argv[]) {
 	vector<double> grid = create_grid(size);
 
 	// Initialize values usefull to compute heast equation
-	double hx = 1./size;
-	double hy = 1./size;
-	double C = 1.;
-	double dt = 0.25*hx*hx/C;
-	double diagx = -2.0 + hx*hx/(2*C*dt);
-	double diagy = -2.0 + hy*hy/(2*C*dt);
-	double weightx = C*dt/(hx*hx);
-	double weighty = C*dt/(hy*hy);
+	auto hx = 1./size;
+	auto hy = 1./size;
+	auto C = 1.;
+	auto dt = 0.25*hx*hx/C;
+	auto diagx = -2.0 + hx*hx/(2*C*dt);
+	auto diagy = -2.0 + hy*hy/(2*C*dt);
+	auto weightx = C*dt/(hx*hx);
+	auto weighty = C*dt/(hy*hy);
 
 	// Compute the heast equation the right number of times
 	for(int iT = 0; iT < max_iter;iT++)
@@ -112,22 +99,22 @@ int main(int argc, char * argv[]) {
 		// Can use par_useq because there is no concurrency problems
 		// since the values of grid aren't modified in computation
 		for_each(std::execution::par_unseq, grid_2.begin(), grid_2.end(),
-			[start = grid_2.data(), size, grid, diagx, diagy, weightx, weighty](double& item) {
+			[start = grid_2.data(), size, grid = grid.data(), diagx, diagy, weightx, weighty](double &item) {
 				// Get and convert index of item of grid_2 into 2d index.
-				int index = &item - start;
+				int index = (int) (&item - start);
 				int x = i2x(index, size);
 				int y = i2y(index, size);
 				// Verify that we aren't in border
 				if (x == 0 || y == 0 || x == size - 1 || y == size - 1)
-					item = grid[index];
+					item = *(grid + index);
 				// Compute heast equation
 				else
-					item = weightx*(grid[xy2i(x-1, y, size)]
-					+ grid[xy2i(x+1, y, size)]
-					+ grid[xy2i(x, y, size)]*diagx)
-					+ weighty*(grid[xy2i(x, y-1, size)]
-					+ grid[xy2i(x, y+1, size)]
-					+ grid[xy2i(x, y, size)]*diagy);
+					item = weightx*(*(grid + xy2i(x-1, y, size))
+					+ *(grid + xy2i(x+1, y, size))
+					+ *(grid + xy2i(x, y, size)) * diagx)
+					+ weighty * (*(grid + xy2i(x, y-1, size))
+					+ *(grid + xy2i(x, y+1, size))
+					+ *(grid + xy2i(x, y, size))*diagy);
 			});
 		// Swap the two grid to have in grid the last state of the grid
 		swap(grid_2, grid);
